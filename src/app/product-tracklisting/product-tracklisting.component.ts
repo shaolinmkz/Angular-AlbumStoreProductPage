8cb1ef2fec52b1ce86f86a844bbfdf0e87dfdeb5
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2019-10-25 17:49:17 +0100

    Abstract  track listing - Create a track listing component - loop through track array and display tracks

---
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Album } from '../album';
+
+@Component({
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
+})
+export class ProductTracklistingComponent implements OnInit {
+
+  constructor(private _productService: ProductService) { }
+  albumInfo: Album;
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
+  }
+
+}
